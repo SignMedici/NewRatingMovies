29de22886233e6d2af3b1785f35e737017992333
Medici <alessandro.accurso@epitech.eu>
2021-12-01 01:21:38 +0100

    style

--- components/Admin/MovieAdmin/index.vue
+++ components/Admin/MovieAdmin/index.vue
@@ -8,7 +8,7 @@
         Modifier metadata
       </nuxt-link>
     </div>
-    <table class="table table-striped borderless text-center">
+    <table class="table table-striped borderless">
       <thead>
         <tr>
           <th class="text-left">
@@ -27,7 +27,6 @@
             Résumé
           </th>
           <th></th>
-          <th></th>
         </tr>
       </thead>
       <tbody v-if="getMovies.length > 0">
@@ -37,21 +36,19 @@
             <td class="noWrap">{{ movie.release_date }}</td>
             <td class="noWrap">{{ movie.director }}</td>
             <td class="overview"><span class="overviewTxt">{{ movie.overview }}</span></td>
-            <td class="align-middle">
-              <div>
+            <td>
+              <div class="d-flex align-center">
                 <nuxt-link :to="{ name: 'admin-movies-updateMovie-id', params: { id: movie._id }, hash: '#logo' }" class="btn">
                   <svg style="width:24px;height:24px;color:#22d157;" viewBox="0 0 24 24">
                     <path fill="currentColor" d="M21.7,13.35L20.7,14.35L18.65,12.3L19.65,11.3C19.86,11.09 20.21,11.09 20.42,11.3L21.7,12.58C21.91,12.79 21.91,13.14 21.7,13.35M12,18.94L18.06,12.88L20.11,14.93L14.06,21H12V18.94M12,14C7.58,14 4,15.79 4,18V20H10V18.11L14,14.11C13.34,14.03 12.67,14 12,14M12,4A4,4 0 0,0 8,8A4,4 0 0,0 12,12A4,4 0 0,0 16,8A4,4 0 0,0 12,4Z" />
                   </svg>
                 </nuxt-link>
+                <button @click="deleteMovie(movie._id)" class="btn removeUnderline">
+                  <svg style="width:24px;height:24px;color:#ad0545" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" />
+                  </svg>
+                </button>
               </div>
-            </td>
-            <td class="align-middle">
-              <button @click="deleteMovie(movie._id)" class="btn removeUnderline">
-                <svg style="width:24px;height:24px;color:#ad0545" viewBox="0 0 24 24">
-                  <path fill="currentColor" d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" />
-                </svg>
-              </button>
             </td>
         </tr>
       </tbody>
--- components/Admin/UserAdmin/index.vue
+++ components/Admin/UserAdmin/index.vue
@@ -8,7 +8,6 @@
           <th scope="col">Adresse Email</th>
           <th scope="col">Admin?</th>
           <th scope="col"></th>
-          <th scope="col"></th>
         </tr>
       </thead>
       <tbody>
@@ -26,14 +25,13 @@
                   </svg>
                 </button>
               </nuxt-link>
+              <button @click="deleteUser(user._id)">
+                <svg style="width:24px;height:24px;color:#ad0545" viewBox="0 0 24 24">
+                  <path fill="currentColor" d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" />
+                </svg>
+              </button>
             </div>
           </td>
-          <td>
-            <button @click="deleteUser(user._id)">
-            <svg style="width:24px;height:24px;color:#ad0545" viewBox="0 0 24 24">
-              <path fill="currentColor" d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" />
-            </svg>
-          </button></td>
         </tr>
       </tbody>
     </table>
