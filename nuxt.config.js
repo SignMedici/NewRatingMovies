90fc0b8d0b12e64e68dfd02e09c091577a868739
Medici <alessandro.accurso@epitech.eu>
2021-12-05 01:12:41 +0100

    change server

--- nuxt.config.js
+++ nuxt.config.js
@@ -128,8 +128,8 @@ export default {
   }, */
 
   env: {
-    baseURL: "http://localhost:8010/api",
-    /* baseURL: "http://newratingmovies-backend.herokuapp.com/api", */
+    /* baseURL: "http://localhost:8010/api", */
+    baseURL: "http://newratingmovies-backend.herokuapp.com/api",
     API_PIC_URL: "http://image.tmdb.org/t/p/w500",
   },
 
@@ -140,8 +140,8 @@ export default {
 
   //Axios
   axios: {
-    baseURL: "http://localhost:8010/api",
-    /* baseURL: "http://newratingmovies-backend.herokuapp.com/api", */
+    /* baseURL: "http://localhost:8010/api", */
+    baseURL: "http://newratingmovies-backend.herokuapp.com/api",
   },
 
   auth: {
