12a95147c06c0f24b05bdb9e89537bc0555edbe6
Medici <alessandro.accurso@epitech.eu>
2021-12-06 18:59:06 +0100

    form padding

--- components/Auth/Login/Index.vue
+++ components/Auth/Login/Index.vue
@@ -1,33 +1,36 @@
 <template>
   <div>
     <UIBigLogo />
-    <v-container class="grey lighten-5 loginForm">
-      <div class="pageLogoTitle">
-        <svg style="width:65px;height:65px" viewBox="0 0 24 24">
-            <path fill="currentColor" d="M12,1L3,5V11C3,16.55 6.84,21.74 12,23C17.16,21.74 21,16.55 21,11V5L12,1M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M17.13,17C15.92,18.85 14.11,20.24 12,20.92C9.89,20.24 8.08,18.85 6.87,17C6.53,16.5 6.24,16 6,15.47C6,13.82 8.71,12.47 12,12.47C15.29,12.47 18,13.79 18,15.47C17.76,16 17.47,16.5 17.13,17Z" />
-        </svg><br>
-        {{ $t('login') }}</div>
-    <form @submit.prevent="login">
-      <div class="mt-4">
-        <input
-          type="email"
-          :placeholder="$t('emailAddress')"
-          class="form-control"
-          id="email"
-          v-model="loginData.email"
-          aria-describedby="emailHelp"
-        />
-        <input
-          type="password"
-          :placeholder="$t('password')"
-          v-model="loginData.password"
-          class="fields form-control"
-          id="password"
-        />
-      </div>
-      <button type="submit" class="btn w-100 confirmButton mt-3">{{ $t('confirm') }}</button>
-    </form>
-    </v-container>
+    <div class="formTopMargin">
+      <v-container class="grey lighten-5 loginForm">
+        <div class="pageLogoTitle">
+          <svg style="width:65px;height:65px" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M12,1L3,5V11C3,16.55 6.84,21.74 12,23C17.16,21.74 21,16.55 21,11V5L12,1M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M17.13,17C15.92,18.85 14.11,20.24 12,20.92C9.89,20.24 8.08,18.85 6.87,17C6.53,16.5 6.24,16 6,15.47C6,13.82 8.71,12.47 12,12.47C15.29,12.47 18,13.79 18,15.47C17.76,16 17.47,16.5 17.13,17Z" />
+          </svg><br>
+          {{ $t('login') }}
+        </div>
+        <form @submit.prevent="login">
+          <div class="mt-4">
+            <input
+              type="email"
+              :placeholder="$t('emailAddress')"
+              class="form-control"
+              id="email"
+              v-model="loginData.email"
+              aria-describedby="emailHelp"
+            />
+            <input
+              type="password"
+              :placeholder="$t('password')"
+              v-model="loginData.password"
+              class="fields form-control"
+              id="password"
+            />
+          </div>
+          <button type="submit" class="btn w-100 confirmButton mt-3">{{ $t('confirm') }}</button>
+        </form>
+      </v-container>
+    </div>
   </div>
 </template>
 
@@ -62,12 +65,15 @@ export default {
 };
 </script>
 <style scoped>
+.formTopMargin{
+  display:block;
+  padding-top: 17px;
+}
 .loginForm{
   width: 600px;
   padding: 30px;
   background-color: #ffffff20;
   border-radius: 14px;
-  margin-top: 20px;
 }
 .pageLogoTitle{
   color: #ffffff79;
--- components/Auth/Register/index.vue
+++ components/Auth/Register/index.vue
@@ -1,43 +1,45 @@
 <template>
   <div>
     <UIBigLogo />
-    <v-container class="grey lighten-5 loginForm">
-      <div class="loginTitle">
-        <svg style="width:65px;height:65px" viewBox="0 0 24 24">
-            <path fill="currentColor" d="M21.7 14.4L20.7 15.4L18.6 13.3L19.6 12.3C19.8 12.1 20.2 12.1 20.4 12.3L21.7 13.6C21.9 13.8 21.9 14.1 21.7 14.4M12 19.9L18.1 13.8L20.2 15.9L14.1 22H12V19.9M10 19.1L21 8.1V5L12 1L3 5V11C3 15.8 5.9 20.3 10 22.3V19.1Z" />
-        </svg><br>
-        {{ $t('register') }}
-      </div>
-      <form @submit.prevent="register" class="mt-3">
-        <input
-          type="text"
-          :placeholder="$t('nickname')"
-          v-model="registerData.nickname"
-          class="form-control"
-          id="nickname"
-        />
-        <input
-          type="email"
-          :placeholder="$t('emailAddress')"
-          class="fields form-control"
-          id="email"
-          v-model="registerData.email"
-          aria-describedby="emailHelp"
-        />
-        <input
-          type="password"
-          :placeholder="$t('password')"
-          v-model="registerData.password"
-          class="form-control"
-          id="password"
-        />
-        <select  class="form-select langField" aria-label="Prefered language" v-model="registerData.language" required>
-          <option selected value="">{{ $t('language') }}</option>
-          <option v-for="locale in this.$i18n.locales" v-bind:value="locale.code">{{ locale.name }}</option>
-        </select>
-        <button type="submit" class="btn w-100 registerButton mt-3">{{ $t('confirm') }}</button>
-      </form>
-    </v-container>
+    <div class="formTopMargin">
+      <v-container class="grey lighten-5 loginForm">
+        <div class="loginTitle">
+          <svg style="width:65px;height:65px" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M21.7 14.4L20.7 15.4L18.6 13.3L19.6 12.3C19.8 12.1 20.2 12.1 20.4 12.3L21.7 13.6C21.9 13.8 21.9 14.1 21.7 14.4M12 19.9L18.1 13.8L20.2 15.9L14.1 22H12V19.9M10 19.1L21 8.1V5L12 1L3 5V11C3 15.8 5.9 20.3 10 22.3V19.1Z" />
+          </svg><br>
+          {{ $t('register') }}
+        </div>
+        <form @submit.prevent="register" class="mt-3">
+          <input
+            type="text"
+            :placeholder="$t('nickname')"
+            v-model="registerData.nickname"
+            class="form-control"
+            id="nickname"
+          />
+          <input
+            type="email"
+            :placeholder="$t('emailAddress')"
+            class="fields form-control"
+            id="email"
+            v-model="registerData.email"
+            aria-describedby="emailHelp"
+          />
+          <input
+            type="password"
+            :placeholder="$t('password')"
+            v-model="registerData.password"
+            class="form-control"
+            id="password"
+          />
+          <select  class="form-select langField" aria-label="Prefered language" v-model="registerData.language" required>
+            <option selected value="">{{ $t('language') }}</option>
+            <option v-for="locale in this.$i18n.locales" v-bind:value="locale.code">{{ locale.name }}</option>
+          </select>
+          <button type="submit" class="btn w-100 registerButton mt-3">{{ $t('confirm') }}</button>
+        </form>
+      </v-container>
+    </div>
   </div>
 </template>
 
@@ -75,6 +77,10 @@ export default {
 </script>
 
 <style scoped>
+.formTopMargin{
+  display:block;
+  padding-top: 17px;
+}
 .loginForm{
   width: 600px;
   padding: 30px;
