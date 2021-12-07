de717c6e687395692d895f788d4af90a8c026382
Medici <alessandro.accurso@epitech.eu>
2021-12-07 02:07:20 +0100

    movie crud correction

--- components/Admin/MovieAdmin/index.vue
+++ components/Admin/MovieAdmin/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div v-if="isAdmin === true">
     <div>
-      <nuxt-link :to="{ name: `movie-add___${locale}`, hash: '#logo' }" class="btn btn-success mb-2 me-2">
+      <nuxt-link :to="{ name: `movie-add___${siteLang}`, hash: '#logo' }" class="btn btn-success mb-2 me-2">
         {{ $t('add') }}
       </nuxt-link>
-      <nuxt-link :to="{ name: `movie-metadata___${locale}`,hash:'#logo'}" class="btn btn-success mb-2">
+      <nuxt-link :to="{ name: `movie-metadata___${siteLang}`,hash:'#logo'}" class="btn btn-success mb-2">
         {{ $t('modifyMetadata') }}
       </nuxt-link>
     </div>
@@ -31,14 +31,14 @@
       </thead>
       <tbody v-if="getMovies.length > 0">
         <tr v-for="movie in getMovies" :key="movie._id">
-            <td >{{ movie.title }}</td>
+            <td >{{ movie[siteLang].title }}</td>
             <td>{{ movie.vote_average }}</td>
             <td class="noWrap">{{ movie.release_date }}</td>
             <td class="noWrap">{{ movie.director }}</td>
-            <td class="overview"><span class="overviewTxt">{{ movie.overview }}</span></td>
+            <td class="overview"><span class="overviewTxt">{{ movie[siteLang].overview }}</span></td>
             <td>
               <div class="d-flex">
-                <nuxt-link class="me-2" :to="{ name: `admin-movies-update-id___${locale}`, params: { id: movie._id }, hash: '#logo' }">
+                <nuxt-link class="me-2" :to="{ name: `admin-movies-update-id___${siteLang}`, params: { id: movie._id }, hash: '#logo' }">
                   <svg style="width:24px;height:24px;color:#22d157;" viewBox="0 0 24 24">
                     <path fill="currentColor" d="M21.7,13.35L20.7,14.35L18.65,12.3L19.65,11.3C19.86,11.09 20.21,11.09 20.42,11.3L21.7,12.58C21.91,12.79 21.91,13.14 21.7,13.35M12,18.94L18.06,12.88L20.11,14.93L14.06,21H12V18.94M12,14C7.58,14 4,15.79 4,18V20H10V18.11L14,14.11C13.34,14.03 12.67,14 12,14M12,4A4,4 0 0,0 8,8A4,4 0 0,0 12,12A4,4 0 0,0 16,8A4,4 0 0,0 12,4Z" />
                   </svg>
@@ -69,7 +69,7 @@ export default {
   data(){
     return {
       baseURL: process.env.baseURL,
-      locale: ''
+      siteLang: ''
     }
   },
   methods: {
@@ -103,8 +103,13 @@ export default {
     }
   },
   created() {
-    this.locale = this.$cookiz.get('siteLang');
-    this.$i18n.setLocale(this.locale);
+    if(this.$cookiz.get('siteLang')){
+      this.siteLang = this.$cookiz.get('siteLang')
+    }
+    else{
+      this.siteLang = 'fr'
+    }
+    this.$i18n.setLocale(this.siteLang);
   }
 };
 </script>
