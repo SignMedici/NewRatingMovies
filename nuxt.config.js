61c50baaa7f3f5445110abcdc649023ce9300d30
Medici <alessandro.accurso@epitech.eu>
2021-12-13 02:43:45 +0100

    vuetify config in comment

--- nuxt.config.js
+++ nuxt.config.js
@@ -186,6 +186,7 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
+    /* customVariables: ["~/assets/variables.scss"], */
     theme: {
       dark: true,
       /* themes: {
