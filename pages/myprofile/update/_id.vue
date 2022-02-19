e4818da5e1ad0971301a862272e030dff1df1813
SignMedici <amenophis170981@gmail.com>
2022-02-19 03:32:31 +0100

    div correction

--- pages/myprofile/update/_id.vue
+++ pages/myprofile/update/_id.vue
@@ -79,18 +79,42 @@
               />
             </div>
             <div>
-              <label for="email" class="form-label">{{ $t('emailAddress') }}</label>
+              <label for="email" class="form-label">{{
+                $t("emailAddress")
+              }}</label>
             </div>
             <div :class="['input-group', isEmailValid()]">
-              <input type="email" class="form-control" id="email" aria-describedby="emailHelp" v-model="email">
+              <input
+                type="email"
+                class="form-control"
+                id="email"
+                aria-describedby="emailHelp"
+                v-model="email"
+              />
             </div>
             <div class="my-3">
-              <label for="myLanguage" class="form-label">{{ $t('language')}}</label>
-              <select  class="form-select langField" name="myLanguage" aria-label="Prefered language" v-model="userLang" required>
-                <option v-for="locale in this.$i18n.locales" v-bind:value="locale.code">{{ $t(locale.name) }}</option>
+              <label for="myLanguage" class="form-label">{{
+                $t("language")
+              }}</label>
+              <select
+                class="form-select langField"
+                name="myLanguage"
+                aria-label="Prefered language"
+                v-model="userLang"
+                required
+              >
+                <option
+                  v-for="locale in this.$i18n.locales"
+                  v-bind:value="locale.code"
+                >
+                  {{ $t(locale.name) }}
+                </option>
               </select>
             </div>
-            <button type="submit" class="btn confirmButton">{{ $t('modify') }}</button>
+            <button type="submit" class="btn confirmButton">
+              {{ $t("modify") }}
+            </button>
+          </div>
         </form>
       </v-container>
     </v-container>
