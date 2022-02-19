cf99756860541a27b35568b69ca33dfc11c4a01b
SignMedici <amenophis170981@gmail.com>
2022-02-19 04:57:48 +0100

    invalid valuefor rate

--- components/UI/ratingStars.vue
+++ components/UI/ratingStars.vue
@@ -17,45 +17,44 @@
 import axios from "axios";
 
 export default {
-  props:["movieDbId","myRates"],
-  data(){
-    return{
-      rate:'',
+  props: ["movieDbId", "myRates"],
+  data() {
+    return {
+      rate: 0,
       baseURL: process.env.baseURL,
-    }
+    };
   },
   methods: {
     async rateMovie(rate) {
       let userId = this.$store.getters.getUserInfo.id;
 
-      if(userId){
-        await this.$store.dispatch('rateMovie', [userId, this.movieDbId, rate]);
-      }
-      else{
-        this.$toast.error(this.$t('pleaseLogoutLogin'));
+      if (userId) {
+        await this.$store.dispatch("rateMovie", [userId, this.movieDbId, rate]);
+      } else {
+        this.$toast.error(this.$t("pleaseLogoutLogin"));
       }
     },
   },
-  created(){
+  created() {
     let found = false;
 
-    if(this.myRates){
-      for(let i = 0; i < this.myRates.length; i++){
-        if (this.myRates[i].movieDbId === this.movieDbId){
+    if (this.myRates) {
+      for (let i = 0; i < this.myRates.length; i++) {
+        if (this.myRates[i].movieDbId === this.movieDbId) {
           this.rate = this.myRates[i].rate / 2;
           found = true;
-          break
+          break;
         }
       }
-      if(!found){
+      if (!found) {
         this.rate = 0;
       }
     }
-  }
-}
+  },
+};
 </script>
 <style scoped>
-.v-card__actions{
+.v-card__actions {
   padding: 0 0 5px 0;
   margin-left: 5px;
 }
