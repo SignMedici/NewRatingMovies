a408c2dcd7f07ae0ae42d9b7d6437fb50d971a71
SignMedici <amenophis170981@gmail.com>
2022-04-10 12:27:53 +0200

    Update: MovieOverview desktop/mobile

--- pages/movies/_id.vue
+++ pages/movies/_id.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="moviePage">
     <UIBigLogo />
-    <MovieOverviewDesktop class="desktop" />
-    <MovieOverviewMobile class="mobile" />
+    <MovieOverviewDesktop v-if="showDesktop" />
+    <MovieOverviewMobile v-if="showMobile" />
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      showDesktop: false,
+      showMobile: false,
+    };
+  },
+  mounted() {
+    let screenSize = window.innerWidth;
+    screenSize >= 1024 ? (this.showDesktop = true) : (this.showMobile = true);
+  },
+};
+</script>
 <style scoped>
 .desktop {
   display: block;
