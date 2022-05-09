6417a0002d65bb8f2cb21c8b7ab4aaeffa3c5c67
SignMedici <amenophis170981@gmail.com>
2022-05-09 23:22:25 +0200

    diable keepAlive

--- plugins/keepAlive.js
+++ plugins/keepAlive.js
@@ -1,7 +1,5 @@
 var http = require("http");
 
 export default () => {
-  setInterval(() => {
-    http.get(process.env.baseURL);
-  }, 1 * 60 * 1000); // every 25 minutes
+  // http.keepAliveTimeout = 1 * 60 * 1000; // every 25 minutes
 };
