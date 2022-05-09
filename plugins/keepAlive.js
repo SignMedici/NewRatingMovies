45fab10c30fd7feb8a66837df2e9086342521f06
SignMedici <amenophis170981@gmail.com>
2022-05-09 22:08:10 +0200

    test: keep backend alive

---
+++ plugins/keepAlive.js
@@ -0,0 +1,25 @@
+var http = require("http");
+
+export default () => {
+  setInterval(function () {
+    var options = {
+      host: process.env.BASE_URL.slice(7, -4),
+      port: 80,
+      path: "/",
+    };
+    http
+      .get(options, function (res) {
+        res.on("data", function (chunk) {
+          try {
+            // optional logging... disable after it's working
+            console.log("HEROKU RESPONSE: " + chunk);
+          } catch (err) {
+            console.log(err.message);
+          }
+        });
+      })
+      .on("error", function (err) {
+        console.log("Error: " + err.message);
+      });
+  }, 20 * 60 * 1000); // load every 20 minutes
+};
