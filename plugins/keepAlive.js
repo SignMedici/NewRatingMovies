01ac369df80920787a6154e0ddb45e9b0c1fb6ed
SignMedici <amenophis170981@gmail.com>
2022-05-09 22:27:51 +0200

    console.log

--- plugins/keepAlive.js
+++ plugins/keepAlive.js
@@ -1,25 +1,8 @@
 var http = require("http");
 
 export default () => {
-  setInterval(function () {
-    var options = {
-      host: process.env.BASE_URL.slice(7, -4),
-      port: 80,
-      path: "/",
-    };
-    http
-      .get(options, function (res) {
-        res.on("data", function (chunk) {
-          try {
-            // optional logging... disable after it's working
-            console.log("HEROKU RESPONSE: " + chunk);
-          } catch (err) {
-            console.log(err.message);
-          }
-        });
-      })
-      .on("error", function (err) {
-        console.log("Error: " + err.message);
-      });
-  }, 1 * 60 * 1000); // load every 20 minutes
+  setInterval(() => {
+    console.log(process.env.BASE_URL);
+    // http.get("http://example.herokuapp.com");
+  }, 1 * 60 * 1000); // every 25 minutes
 };
