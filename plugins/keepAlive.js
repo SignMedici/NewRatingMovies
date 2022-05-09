8c964885f23b3ab0142016d9d032bfcf2e75306e
SignMedici <amenophis170981@gmail.com>
2022-05-09 23:02:47 +0200

    test3 - keep backend alive

--- plugins/keepAlive.js
+++ plugins/keepAlive.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 export default () => {
   setInterval(() => {
-    http.get(process.env.baseURL.slice(0, -3));
+    http.get(process.env.baseURL);
   }, 1 * 60 * 1000); // every 25 minutes
 };
